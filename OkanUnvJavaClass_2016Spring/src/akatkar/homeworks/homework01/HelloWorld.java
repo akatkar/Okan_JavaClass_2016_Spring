65303361871d6914b5c6a2a7ebdb012430e6b207
akatkar <akatkar@ALI-KATKAR.netas.lab.nortel.com>
2016-03-03 22:29:10 +0200

    1.Ödevin Çözümü

---
+++ OkanUnvJavaClass_2016Spring/src/akatkar/homeworks/homework01/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akatkar.homeworks.homework01;
+
+/**
+ *
+ * @author akatkar
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        int var1 = 5;
+        double var2 = 3.12;
+        
+        var1 = (int)var2;
+        System.out.println("var1:"+var1);
+    }
+}
